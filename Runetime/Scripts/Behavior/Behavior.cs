9e9f0c4bb8149df89f9836e02b4ef723db54bfa2
Jared Goronkin <jgoronkin@gmail.com>
2024-08-12 10:20:28 -0400

    updated names

--- Runetime/Scripts/Behavior/Behavior.cs
+++ Runetime/Scripts/Behavior/Behavior.cs
@@ -7,49 +7,49 @@ namespace Mosaic
     public class Behavior : ScriptableObject
     {
         [SerializeField]
-        private GameObject _moduleState;//This gameobject contains all of the logic for the state of the character. It's a gameobject so it can be literaly anything as long as it has a script that dervives from StateInstance
+        private GameObject _instance;//This gameobject contains all of the logic for the behavior of the character. It's a gameobject so it can be literaly anything as long as it has a script that dervives from Instance
         [SerializeField]
-        private List<BehaviorType> _state;
+        private List<BehaviorType> _behaviorType;
         [SerializeField]
-        private List<DecisionData> _decisionData;    //deffine how the state is entered, method to enable the state to exit
+        private List<DecisionData> _decisionData;    //deffine how the behavior is entered, method to enable the Behavior to exit
         [SerializeField]
         private int _priority = 100;
 
-        public GameObject ModuleState => _moduleState;
-        public List<BehaviorType> State => _state;
+        public GameObject Instance => _instance;
+        public List<BehaviorType> BehaviorTypes => _behaviorType;
 
-        private float GetDecisionValue(ICore character, List<BehaviorType> currentState, BehaviorInputType currentInput)//Get how likeley this decision is to occur
+        private float GetDecisionValue(ICore core, List<BehaviorType> currentBehaviorType, BehaviorInputType currentInputType)//Get how likeley this decision is to occur
         {
             float decisionValue = 0;
             foreach (DecisionData data in _decisionData)
             {
-                decisionValue = Mathf.Max(decisionValue, data.GetDecisionValue(character, currentState, currentInput));
+                decisionValue = Mathf.Max(decisionValue, data.GetDecisionValue(core, currentBehaviorType, currentInputType));
             }
             return decisionValue;
         }
-        public static Behavior DecideNewState(List<Behavior> states, ICore character, List<BehaviorType> currentState, BehaviorInputType currentInput)
+        public static Behavior DecideNewBehavior(List<Behavior> behaviors, ICore core, List<BehaviorType> currentBehaviorType, BehaviorInputType currentInputType)
         {
-            Behavior finalState = null;
+            Behavior finalBehavior = null;
             float finalValue = 0;
-            foreach (Behavior checkState in states)
+            foreach (Behavior checkBehavior in behaviors)
             {
-                float checkValue = checkState.GetDecisionValue(character, currentState, currentInput);
+                float checkValue = checkBehavior.GetDecisionValue(core, currentBehaviorType, currentInputType);
 
 
                 if (checkValue > finalValue)
                 {
-                    finalState = checkState;
+                    finalBehavior = checkBehavior;
                     finalValue = checkValue;
                 }
-                else if (finalState != null && checkValue == finalValue)
+                else if (finalBehavior != null && checkValue == finalValue)
                 {
-                    if (checkState._priority == finalState._priority)
+                    if (checkBehavior._priority == finalBehavior._priority)
                     {
-                        throw new System.Exception("Can't decide between multiple states with the same decision value, and the same priority");
+                        throw new System.Exception("Can't decide between multiple behaviors with the same decision value, and the same priority");
                     }
-                    else if (checkState._priority > finalState._priority)
+                    else if (checkBehavior._priority > finalBehavior._priority)
                     {
-                        finalState = checkState;
+                        finalBehavior = checkBehavior;
                     }
                 }
 
@@ -57,45 +57,45 @@ namespace Mosaic
 
             }
 
-            return finalState;
+            return finalBehavior;
         }
 
 
 
 
         [System.Serializable]
-        protected class DecisionData// decide if the state is available for transfer, and give it a score of 0 to 1
+        protected class DecisionData// decide if the behavior is available for transfer, and give it a score of 0 to 1
         {
 
             [SerializeField]
-            private List<BehaviorType> _prevState;// if it's in this state, the transition is possible
+            private List<BehaviorType> _prevBehavior;// if it's in this behavior, the transition is possible
             [SerializeField]
             private List<BehaviorInputType> _validInput;//if the input is valid, the transistion is possible
             [SerializeField]
             private List<BaseDecisionAlgorithm> _decisionAlgorithms;//if the decision value is greater than 0 the transition is possible. All Decision values are multiplied together to gather the final value. The highest value is activated.
-            public float GetDecisionValue(ICore character, List<BehaviorType> currentStates, BehaviorInputType currentInput)
+            public float GetDecisionValue(ICore core, List<BehaviorType> currentBehaviors, BehaviorInputType currentInputType)
             {
                 float decisionValue = 1;
 
-                bool containsState = _prevState.Contains(null);
-                if(containsState)
+                bool containsBehavior = _prevBehavior.Contains(null);
+                if(containsBehavior)
                 {
                     Debug.LogWarning("Previos behavior includes null value.");
                 }
 
-                foreach (BehaviorType s in currentStates)
+                foreach (BehaviorType s in currentBehaviors)
                 {
-                    containsState = containsState || _prevState.Contains(s);
+                    containsBehavior = containsBehavior || _prevBehavior.Contains(s);
 
                 }
 
-                decisionValue *= containsState ? 1 : 0;
+                decisionValue *= containsBehavior ? 1 : 0;
 
-                decisionValue *= _validInput.Contains(currentInput) ? 1 : 0;
+                decisionValue *= _validInput.Contains(currentInputType) ? 1 : 0;
 
                 foreach (BaseDecisionAlgorithm decisionAlgorithm in _decisionAlgorithms)
                 {
-                    decisionValue *= decisionAlgorithm.CheckDecesion(character);
+                    decisionValue *= decisionAlgorithm.CheckDecesion(core);
                 }
 
                 return decisionValue;
---
+++ Runetime/Scripts/Input/CoreInput.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Mosaic
+{
+    /// <summary>
+    /// One of the two default components required for a character to function.
+    /// </summary>
+    [RequireComponent(typeof(ICore))]
+    public abstract class CoreInput : MonoBehaviour
+    {
+        protected BehaviorInstance BehaviorInstance;
+
+        public void OverrideControl(BehaviorInstance behaviorInstance)
+        {
+
+            if (this.BehaviorInstance != null)
+            {
+                this.BehaviorInstance.ControlEnd();
+            }
+
+            this.BehaviorInstance = behaviorInstance;
+
+            if (behaviorInstance != null)
+            {
+                behaviorInstance.ControlStart();
+            }
+        }
+    }
+
+}
